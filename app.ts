273859cd8c335acf8e0c6e8329d09d17bde75fa0
MrWoodpecker <arp.christian@yahoo.com>
2022-01-07 21:33:21 +0100

    initial commit
    get this started :smile:

---
+++ app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+
+    res.send('¡Hola Señor!');
+
+});
+
+
+
+app.listen(port, () => {
+
+    return console.log(`Server is listening on ${port}`);
+
+});
